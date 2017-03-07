4c3eeaf795a80a642d077dd0a47b227283c270b9
Jorge Perales Diaz <slipknot_jpd@hotmail.com>
2017-03-06 21:17:27 -0600

    Basic live recognition
    This program use OpenCv for live Capture, and libraries like
    OxfordProjects to recognize your face and do an analysis.

---
+++ LiveCameraSample/CelebritiesResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+namespace LiveCameraSample
+{
+    public class CelebritiesResult
+    {
+        public Celebrity[] Celebrities { get; set; }
+    }
+
+    public class Celebrity
+    {
+        public string Name { get; set; }
+        public FaceRectangle FaceRectangle { get; set; }
+        public float Confidence { get; set; }
+    }
+}
